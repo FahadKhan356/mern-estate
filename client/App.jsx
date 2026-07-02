2fd297d68496f526fa7ed00080b7bffea15d27a1
farhanali355 <fahadk8080@gmail.com>
2026-07-03 02:41:38 +0500

    created Pages and design header

---
+++ client/App.jsx
@@ -0,0 +1,25 @@
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import Header from './componenets/Header'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignIn/>}/>
+        <Route path="/about" element={<About/>}/>
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/pages/Home.jsx
@@ -0,0 +1,9 @@
+
+
+function Home() {
+  return (
+    <div>asadasdsadSss</div>
+  )
+}
+
+export default Home
